d43f9ce102f8b78dfa23a245cb87009e58136664
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-09 18:22:33 -0300

    Finally, show.cpp is done.

--- show.cpp
+++ show.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
 #include <string>
-#include <OpenImageIO/imagecache.h>
+#include <sstream>
 
-namespace oiio = OpenImageIO::v1_2;
+#include <OpenImageIO/imageio.h>
+#include <OpenImageIO/imagebuf.h>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+OIIO_NAMESPACE_USING
+
+#define BUFF_SIZE 400
 
 int main(int argc, char* args[])
 {
-    /*
-    if (argc != 2)
+    if (argc != 3)
     {
         return 1;
     }
-    */
-
-    const std::string imagesDirectory = "/home/ramiro/Imagens/"; //args[1];
-
-
-
-    // Create an image cache and set some options
-    oiio::ImageCache *cache = oiio::ImageCache::create();
-    cache->attribute("max_memory_MB", 40000); //40GB
-    cache->attribute("searchpath", imagesDirectory);
-
-    std::string filePath = "0.pgm";
-
-    oiio::ImageSpec spec;
-    bool ok = cache->get_imagespec(oiio::ustring(filePath), spec); //not able to use std::string???
-    if ( !ok )
+    const std::string filePath = args[1];
+    int index;
     {
-        return 2;
+        std::stringstream ss;
+        ss << args[2];
+        ss >> index;
     }
 
+    ImageInput *in = ImageInput::open (filePath);
+    const ImageSpec & spec = in->spec();
+    const imagesize_t size = (imagesize_t)spec.width * spec.height; //Notice one of the rvalues must be of type
+                                                                    //imagesize_t, or the result will be coerced to int
+                                                                    //and be a lot smaller than needed
+    unsigned char * pixels = new unsigned char[size];
+    in->read_image(TypeDesc::UCHAR, pixels);
+    in->close();
+    std::cout << "Done reading." << std::endl;
 
-    unsigned char pixels[400]; //width * height * channels of the samples = 20*20*1
+    cv::Mat image(spec.height, spec.width, CV_8UC1, pixels);
+    cv::Rect roi(index * 20, 0, 20, 20);
 
-    int i = 0;
-    for(int x = 0; x < spec.width; x += 20, i++)
-    {
-        cache->get_pixels (oiio::ustring(filePath),
-                       0, 0,
-                       x, x + 20,
-                       0, 20,
-                       0, 0,
-                       oiio::TypeDesc::UCHAR,
-                       pixels);
-    }
-
-    oiio::ImageCache::destroy(cache);
-
-    std::cout << i;
+    cv::imshow("Selected ROI", image(roi));
+    cv::waitKey(0);
 
     return 0;
 }
