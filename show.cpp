00a7a53302621a052d993b2b31a1fe4227a96869
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-16 15:15:54 -0300

    Stopped to use the OpenImageIO in show.cpp.

--- show.cpp
+++ show.cpp
@@ -3,18 +3,12 @@
 #include <sstream>
 #include <fstream>
 
-#include <OpenImageIO/imageio.h>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-OIIO_NAMESPACE_USING
-
-#define BUFF_SIZE 400
-
 int main(int argc, char* args[])
 {
-    if (argc != 3)
+    if (argc < 3)
     {
         return 1;
     }
@@ -35,17 +29,8 @@ int main(int argc, char* args[])
         in.close();
     }
 
-    ImageInput *in = ImageInput::open (filePath);
-    const ImageSpec & spec = in->spec();
-    const imagesize_t size = (imagesize_t)spec.width * spec.height; //Notice one of the rvalues must be of type
-                                                                    //imagesize_t, or the result will be coerced to int
-                                                                    //and be a lot smaller than needed
-    cv::Mat image(spec.height, spec.width, CV_8UC1);
-    in->read_image(TypeDesc::UCHAR, image.data); //writes directly on the matrix buffer. It seems it works.
-    in->close();
-    std::cout << "Done reading." << std::endl;
-
-    cv::Rect roi(index * 20, 0, 20, 20);
+    cv::Mat image = cv::imread(filePath, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Rect roi(index * 20, 0, 1000, 20);
 
     cv::imshow("Selected ROI", image(roi));
     cv::waitKey(0);
